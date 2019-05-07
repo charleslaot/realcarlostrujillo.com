6ed9adc60900aba207206927e9b6397ac3046612
carlostrujillo90 <krloslao90@gmail.com>
2019-05-07 13:51:57 -0400

    Updating resume

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,5 +1,22 @@
 (function($) {
 
+	function animateCSS(element, animationName, callback) {
+		const node = document.querySelector(element)
+		node.classList.add('animated', animationName)
+	
+		function handleAnimationEnd() {
+			node.classList.remove('animated', animationName)
+			node.removeEventListener('animationend', handleAnimationEnd)
+	
+			if (typeof callback === 'function') callback()
+		}
+	
+		node.addEventListener('animationend', handleAnimationEnd)
+	}
+
+	
+	// animateCSS('.fa-cogs h3', 'bounce')
+
 	/**
 	 * Generate an indented list of links from a nav. Meant for use with panel().
 	 * @return {jQuery} jQuery object.
